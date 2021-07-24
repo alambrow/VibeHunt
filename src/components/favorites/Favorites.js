ee7a8a50432c0da177bde654d00c7c89957eb62b
alambrow <alexander.lambrow@gmail.com>
2021-07-24 13:22:40 -0500

    still debugging ranking system

---
+++ src/components/favorites/Favorites.js
@@ -0,0 +1,7 @@
+export const Favorites = () => {
+    return (
+        <>
+        <p>This page under construction</p>
+        </>
+    )
+}
--- src/components/mobile.js
+++ src/components/mobile.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import { NavBar } from './nav/NavBar';
 import { VenueWall } from './venue_wall/VenueWall';
+import { Favorites } from './favorites/Favorites';
 
 export const Mobile = () => {
     return (
@@ -9,6 +10,9 @@ export const Mobile = () => {
         <NavBar />
         <Route exact path="/">
             <VenueWall />
+        </Route>
+        <Route exact path="/favorites">
+            <Favorites />
         </Route>
     </>
     )
